96b4fc23276e7f53b32cec14a4c27344f5d660fa
Jonathan Conrad <jc@Jonathans-MBP.home>
2022-06-28 08:47:25 -0700

    Added SearchBar component as well as empty component for VideoPlayer

---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const SearchBar = (props) => {
+
+    const [searchQuery, setSearchQuery] = useState('');
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        let mySearchQuery = searchQuery.toLocaleLowerCase();
+        console.log(mySearchQuery);
+        props.searchQueryData(mySearchQuery);
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <input type='str' value={searchQuery} placeholder='Search...' onChange={(event) => setSearchQuery(event.target.value)}/>
+            </div>
+            <div>
+                <button type='submit'></button>
+            </div>
+        </form>
+    )
+}
+
+export default SearchBar;
